baccd1617214dad3d263720a54594e161a83ea2a
Avi Bryant <avi@dabbledb.com>
2010-04-04 18:58:59 -0700

    sketching out what method and class defs might look like

--- 8ball.rb
+++ 8ball.rb
@@ -1,49 +1,64 @@
 test_code = <<EOF
-   def zot
-       3+4
-   end
+	class Foo
+		def zot
+			3+4
+		end
+	end
 EOF
 
 require "java"
 
-class JavascriptVisitor
-  include org.jruby.ast.visitor.NodeVisitor
+class EightBallVisitor
+	include org.jruby.ast.visitor.NodeVisitor
 
-  def visit(node)
-    node.accept(self)
-  end
+	def visit(node)
+		node.accept(self)
+	end
 
-  def visitRootNode(node)
-    visit(node.bodyNode)
-  end
+	def visitRootNode(node)
+		visit(node.bodyNode)
+	end
 
-  def visitNewlineNode(node)
-    visit(node.nextNode)
-  end
+	def visitNewlineNode(node)
+		visit(node.nextNode)
+	end
 
-  def visitDefnNode(node)
-    print "\nfunction " + node.getName + "() {\n"
-    print "return "
-    visit(node.bodyNode)
-    print "\n}"
-  end
+	def visitCallNode(node)
+		visit(node.receiverNode)
+		print ".#{mangle(node.getName)}("
+		visit(node.argsNode.getLast)
+		print ")"
+	end
 
-  def visitCallNode(node)
-    visit(node.receiverNode)
-    print " #{node.getName} "
-    visit(node.argsNode.getLast)
-  end
-
-  def visitFixnumNode(node)
-    print node.value
-  end
+	def visitFixnumNode(node)
+		print "(#{node.value})"
+	end
+	
+	def mangle(method)
+		method.gsub("+", "$plus")
+	end
 end
 
-test_code = File.read("/Users/mknox/twitter/lib/decider.rb")
+class ClassDefVisitor < EightBallVisitor
+	def visitClassNode(node)
+		puts "this.define_class(\"#{node.getCPath.getName}\","
+		puts "function() {"
+		ClassDefVisitor.new.visit(node.bodyNode)
+		puts "})"
+	end
+	
+	def visitDefnNode(node)
+		puts "this.define_method(\"#{node.getName}\","
+		puts "function() {"
+		print "return "
+		MethodDefVisitor.new.visit(node.bodyNode)
+		puts "})"
+	end
+end
+
+class MethodDefVisitor < EightBallVisitor
+end
+
 r = org.jruby.Ruby.getDefaultInstance
-ast = r.parse(test_code, "test_code",
-r.getCurrentContext.getCurrentScope, 0, true)
-File.open("jruby_example", "w").do |f|
-  f.write ast.inspect
-end
-ast.accept(JavascriptVisitor.new)
+ast = r.parse(test_code, "test_code", r.getCurrentContext.getCurrentScope, 0, true)
+ast.accept(ClassDefVisitor.new)
