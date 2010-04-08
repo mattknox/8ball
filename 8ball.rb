2bc5a1b28060fa4d134d4b7f8590ccad8863b219
matt knox <matthewknox@gmail.com>
2010-04-07 23:11:18 -0700

    got function calls working with real bodies and arguments.

--- 8ball.rb
+++ 8ball.rb
@@ -1,68 +1,140 @@
-test_code = <<EOF
-	class Foo
-		def zot
-			3+4
-		end
-	end
-EOF
-
 require "java"
+require 'pp'
 
-class EightBallVisitor
-	include org.jruby.ast.visitor.NodeVisitor
+class String
+  def first
+    self[0].chr
+  end
 
-	def visit(node)
-		node.accept(self)
-	end
+  def last
+    self[-1].chr
+  end
 
-	def visitRootNode(node)
-		visit(node.bodyNode)
-	end
-
-	def visitNewlineNode(node)
-		visit(node.nextNode)
-	end
-
-	def visitCallNode(node)
-		visit(node.receiverNode)
-		print ".#{mangle(node.getName)}("
-		visit(node.argsNode.getLast)
-		print ")"
-	end
-
-	def visitFixnumNode(node)
-		print "(#{node.value})"
-	end
-
-	def mangle(method)
-		method.gsub("+", "$plus")
-	end
-end
-
-class ClassDefVisitor < EightBallVisitor
-	def visitClassNode(node)
-		puts "this.define_class(\"#{node.getCPath.getName}\","
-		puts "function() {"
-		ClassDefVisitor.new.visit(node.bodyNode)
-		puts "})"
-	end
-
-	def visitDefnNode(node)
-		puts "this.define_method(\"#{node.getName}\","
-		puts "function() {"
-		print "return "
-		MethodDefVisitor.new.visit(node.bodyNode)
-		puts "})"
-	end
-end
-
-class MethodDefVisitor < EightBallVisitor
-end
-
-class EightBallCompiler
-  def self.compile_string(ruby)
-    r = org.jruby.Ruby.getDefaultInstance
-    ast = r.parse(ruby, "test_code", r.getCurrentContext.getCurrentScope, 0, true)
-    ast.accept(ClassDefVisitor.new)
+  def wrap_with(wrapper)
+    if wrapper.length == 2
+      wrapper.first + self + wrapper.last
+    elsif wrapper.length == 1
+      wrapper + self + wrapper
+    else
+      raise "bad argument"
+    end
   end
 end
+
+class Array
+  def to_comma_list
+    self.join(", ").wrap_with("()")
+  end
+end
+
+class EightBallVisitor
+  include org.jruby.ast.visitor.NodeVisitor
+
+  def visit(node)
+    node.accept(self)
+  end
+
+  def visitRootNode(node)
+    visit(node.bodyNode)
+  end
+
+  def visit_block_node(node)
+    node.child_nodes.map { |x| visit(x) }
+  end
+
+  def visitNewlineNode(node)
+    visit(node.nextNode)
+  end
+
+  def visitCallOneArgNode(node)
+    visitCallNode(node)
+  end
+
+  def visitCallNode(node)
+    mangle(node.getName) +
+      "#{visit(node.receiverNode)},#{visit(node.argsNode.getLast).to_s}".wrap_with("()")
+  end
+
+  def visitFixnumNode(node)
+    "#{node.value}"
+  end
+
+  def visitStrNode(node)
+    node.value.wrap_with('"')
+  end
+
+  def mangle(method)
+    method.gsub("+", "primplus") # might need this later, leaving it for now.
+  end
+
+  def visitClassNode(node)
+    "this.define_class(\"#{node.getCPath.getName}\"," +
+      "function() {" +
+      visit(node.bodyNode) +
+      "})"
+  end
+
+  def visitDefnNode(node)
+    compile_function(node.get_name, node.args_node.args, node.body_node)
+  end
+
+  def visitArgsNode(node)
+    node.args.child_nodes.to_a.map {|x| x.name}.join(", ")
+  end
+
+  def visitLocalVarNode(node)
+    node.get_name
+  end
+
+  def compile_function(name, args, body)
+    "function #{name}#{compile_arglist(args)}" + compile_function_body(body).wrap_with("{}")
+  end
+
+  def compile_arglist(node)
+    node.child_nodes.to_a.map {|x| x.name}.to_comma_list
+  end
+
+  def compile_function_body(node)
+    if node.is_a? Java::OrgJrubyAst::BlockNode
+      [node.child_nodes.to_a[0..-2].map { |n| visit(n) } +
+       ["return #{visit(node.child_nodes.to_a.last)}"]].join(";\n")
+    else
+      "return #{visit(node)}"
+    end
+  end
+end
+
+
+class EightBallCompiler
+  def self.parse(ruby)
+    r = org.jruby.Ruby.getDefaultInstance
+    r.parse(ruby, "test_code", r.getCurrentContext.getCurrentScope, 0, true)
+  end
+
+  def self.compile_string(ruby)
+    puts "we're about to compile\n\n#{ruby}\n"
+    pp (ast = parse(ruby))
+    puts "\n\n"
+    puts ast.accept(EightBallVisitor.new)
+  end
+end
+
+# stuff to aid in development
+# I like dumping this into the jirb environment with
+#  eval(File.read("8ball.rb"))
+
+e = EightBallCompiler
+v = EightBallVisitor.new
+
+avi = File.read("target/avi.rb")
+control = File.read("target/control.rb")
+d = File.read("target/def.rb")
+klass = File.read("target/klass.rb")
+simple = File.read("target/simple.rb")
+target = File.read("target/target.rb")
+x = File.read("target/x.rb")
+
+ast = e.parse(simple)
+d = ast.child_nodes.first.child_nodes.first
+matt = ast.child_nodes.first.child_nodes.first.child_nodes.first
+av = ast.child_nodes.first.child_nodes.to_a.last
--- target/simple.rb
+++ target/simple.rb
@@ -1,3 +1,9 @@
-def matt(a, b)
-  a + b
+def matt(a, b, c, d)
+  "foo"
+  "bar"
+  a + b + c + d
+end
+
+def avi(x,y)
+  x + y
 end
