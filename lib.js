a86771dc31091487cd3c292d723f281924b47749
Ben Matasar <ben@matasar.org>
2010-04-16 09:15:46 -0700

    I had missed lib.js in favor of primitives.js.

--- lib.js
+++ lib.js
@@ -1,2 +1,4 @@
  Number.prototype.bm = function(f) { var acc = ""; for(var i = 0; i < this; i++){ acc += i; } return acc; };
  Number.prototype.times = function(f) { for(var i = 0; i < this; i++){ f(i); } return this; };
+ Number.prototype.primplus = function(y) { return this + y; };
+
--- primitives.js
+++ primitives.js
@@ -1,4 +1,1 @@
-Number.prototype.primplus = function(y) { return this + y; };
-Number.prototype.times = function (fn) { for (var i = 0; i < this; i++) { fn(i) }}
-
 function puts(string) {console.log(string)};
