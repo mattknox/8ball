f23df3df3b9cba7d95404d1d005d7fe141ad39c4
matt knox <matthewknox@gmail.com>
2010-04-19 17:20:19 -0700

    moving the cgi-bin and js stuff

--- lib.js
+++
@@ -1,6 +0,0 @@
- Number.prototype.bm = function(f) { var acc = ""; for(var i = 0; i < this; i++){ acc += i; } return acc; };
- Number.prototype.times = function(f) { for(var i = 0; i < this; i++){ f(i); } return this; };
- Number.prototype.primplus = function(y) { return this + y; };
-
-  // http://peter.michaux.ca/articles/the-command-pattern-in-javascript-encapsulating-function-property-calls
-  // http://wiki.github.com/ry/node/modules
--- primitives.js
+++
@@ -1,5 +0,0 @@
-function puts(string) {console.log(string)};
-
-function add(a,b) {
-    return a + b;
-}
