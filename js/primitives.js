4b5e59768cba412ef685a2c754bfe77ea64c424e
matt knox <matthewknox@gmail.com>
2010-04-25 16:00:33 -0700

    fixed syntax errors in primitives.js

--- js/primitives.js
+++ js/primitives.js
@@ -1,10 +1,9 @@
-function puts(string) {console.log(string)};
+function puts(string) {console.log(string);};
 
 function add(a,b) {
     return a + b;
 }
 /* these should eventually do ruby-style case dispatch, and should be replaced
  with calls to naked js primitives when possible. */
-Number.prototype.rubyplus = function(a) { return a + this }; 
-Number.prototype.rubyplus = function(a) { return a - this };
-
+Number.prototype.rubyplus = function(a) { return a + this; }; 
+Number.prototype.rubyplus = function(a) { return a - this; };
