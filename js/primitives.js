52a543bc8c81c7c47204e3c261399a53bdf2a60e
matt knox <matthewknox@gmail.com>
2010-04-25 16:56:26 -0700

    added rubytruthy to handle cases in which we want to do short-circuiting eval, eg: a && b .  js has bizarre semantics in that 0, '', and false all == false.

--- js/primitives.js
+++ js/primitives.js
@@ -6,4 +6,5 @@ function add(a,b) {
 /* these should eventually do ruby-style case dispatch, and should be replaced
  with calls to naked js primitives when possible. */
 Number.prototype.rubyplus = function(a) { return a + this; }; 
-Number.prototype.rubyplus = function(a) { return a - this; };
+Number.prototype.rubyminus = function(a) { return a - this; };
+Object.prototype.rubytruthy = function () { return (this == null || (typeof(this) == 'boolean' && this == false)) ? false : this; };
