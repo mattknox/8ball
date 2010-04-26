6a9604a217c519c05d87ca68340eed23f3f37b34
matt knox <matthewknox@gmail.com>
2010-04-25 17:04:28 -0700

    changes to rubytruthy, because you can't call methods on a literal null.

--- js/primitives.js
+++ js/primitives.js
@@ -7,4 +7,4 @@ function add(a,b) {
  with calls to naked js primitives when possible. */
 Number.prototype.rubyplus = function(a) { return a + this; }; 
 Number.prototype.rubyminus = function(a) { return a - this; };
-Object.prototype.rubytruthy = function () { return (this == null || (typeof(this) == 'boolean' && this == false)) ? false : this; };
+Object.prototype.rubytruthy = function (x) { return (x == null || x === false) ? false : x; };
