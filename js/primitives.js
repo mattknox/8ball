f0fe2f74dad9eef45804908b9cdf41feb6cb8cfd
matt knox <matthewknox@gmail.com>
2010-04-28 22:48:59 -0700

    got rubyplus to work for arrays, strings, and numbers.

--- js/primitives.js
+++ js/primitives.js
@@ -1,14 +1,11 @@
-function puts(string) { print(string);};
+var sys = require('sys')
+function puts(string) { sys.puts(string);};
 
-function add(a,b) {
-    return a + b;
-}
 /* these should eventually do ruby-style case dispatch, and should be replaced
  with calls to naked js primitives when possible. */
 Number.prototype.rubyplus = function(a) { return a + this; }; 
+Object.prototype.rubyplus = function(a) { return this.concat(a); }; 
+String.prototype.rubyplus = function(a) { return this + a; }; 
 Number.prototype.rubyminus = function(a) { return a - this; };
 Object.prototype.rubytruthy = function (x) { return (x == null || x === false) ? false : x; };
-
-print(3);
-print('foo');
 
